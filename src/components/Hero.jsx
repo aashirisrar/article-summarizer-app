47556cf8a8e744af6eaa42fae45ccad6b59c16df
Aashir Israr <63807168+aashirisrar@users.noreply.github.com>
2023-07-21 12:05:10 +0500

    initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      Hero
+    </header>
+  );
+};
+
+export default Hero;
