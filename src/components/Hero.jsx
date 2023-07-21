e0eef614736c580682f56ce091481d5c56e2787d
Aashir Israr <63807168+aashirisrar@users.noreply.github.com>
2023-07-21 12:22:32 +0500

    completed the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,26 @@ import { logo } from "../assets";
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-      Hero
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+
+        <button
+          type="button"
+          className="black_btn"
+          onClick={() => window.open("/")}
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-mid:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with Summize, and open-source article summarizer
+        that transforms lengthy articles into clear andsconise summaries
+      </h2>
     </header>
   );
 };
